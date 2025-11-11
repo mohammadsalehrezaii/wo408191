7a035107d2605bfb106c2f16eb60bab5f45491f5
mohammadsalehrezaii <mohammadsalehr580@gmail.com>
2025-11-11 11:18:33 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+// let ege = 18;
+// let lisens = "قبول";
+// if (ege >= 18 && lisens == "قبول") {
+//     console.log("شما میتوانید رانندگی کنید")
+// } else {
+//     console.log("شما قادر به گرفتن گواهینامه نیستید")
+// }
+
+// let hour = 14.5;
+// let pnalty = true;
+
+// if (hour >= 14) {
+//     if (pnalty == true) {
+//         console.log("کلاس ادامه داره");
+//     } else if (pnalty == false) {
+//         console.log("با خیال راحت بروید استراحت کنید")
+//     }
+// } else {
+//     console.log("تا ساعت 14 کلاس هست همیشه")
+// }
+
+// let nodemderofdayoofweek = 7;
+// switch (nodemderofdayoofweek) {
+//     case 1:
+//         console.log("شنبه")
+//         break;
+//     case 2:
+//         console.log("یکشنبه")
+//         break;
+//     case 3:
+//         console.log("دوشنبه")
+//         break;
+//     case 4:
+//         console.log("سه شنبه")
+//         break;
+//     case 5:
+//         console.log("چهار شنبه")
+//         break;
+//     case 6:
+//         console.log("پنجشنبه")
+//         break;
+//     case 7:
+//         console.log("جمعه")
+//         break;
+//     default:
+//         console.log("همچین روزی در هفته نداریم")
+// }
+// let nomberofdayofweek =5;
+// switch(nomberofdayofweek) {
+//     case 6:
+//         case 7:
+//             console.log("برید عشق و حال")
+// break;
+// case 5:
+//     console.log("اخ جون تعطیلیم")
+//     break;
+//     default:
+//         console.log("حیف شد باید برم درس بخونم")
+// }
+
+// for (let i = 1; i <= 20; i++)
+//     if (i % 5 !== 0) {
+//         { console.log(i) };
+//     } else {
+//         console.log("hope");
+//     }
+
+// const student =(
+//     ["Ali" , "computer" , 19],
+//     ["mamad ","math" , 15],
+//     ["max" , "since" , 12],
+//     ["cat", "computer" , 20 ]
+// );
+// for( let i=0; i<student.length; i++){
+//     if( student[i][2] > 10){
+//         console.log("name:" + student[i][0] + ", field: " + student[i][1] + ", score " + student[i][2]);
+//     }
+// }
+
+for (let i=0; i <=20; i++) {
+    if (i % 3 === 0 && i % 5 ===0) {
+        console.log(i, "cointrap");
+    } else if(i % 5 ===0) {
+        console.log(i, "trap");
+    } else if (i % 3 === 0) {
+        console.log(i, "coin")
+    }
+    }
+
+
+
+
+
+
+
+        
